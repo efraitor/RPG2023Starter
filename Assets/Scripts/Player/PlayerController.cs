3e7cd52f33a2ded542849b7e37a916d30ded33a8
efraitor_ <efraitor.contact@gmail.com>
2023-03-20 16:35:59 +0100

    Changes
    - Checkpoints finished

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,73 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    
+    //Velocidad de movimiento del jugador
     public float moveSpeed;
 
+    //Referencia al RigidBody del jugador
     private Rigidbody2D theRB;
-    //public Animator myAnim;
-
-    //public string areaTransitionName;
-    //private Vector3 bottomLeftLimit;
-    //private Vector3 topRightLimit;
-
-    //public bool canMove = true;
-
-    //public static PlayerController instance;
+    //Referencia al Animator del jugador
+    private Animator anim;
 
     // Use this for initialization
     void Start()
     {
-        //if (instance == null)
-        //{
-        //    instance = this;
-        //}
-        //else
-        //{
-        //    if (instance != this)
-        //    {
-        //        Destroy(gameObject);
-        //    }
-        //}
-
-        //DontDestroyOnLoad(gameObject);
-
+        //Inicializamos el RigidBody del jugador
         theRB = GetComponent<Rigidbody2D>();
+        //Inicializamos el Animator del jugador
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //    if (canMove)
-        //    {
+        //Movemos al personaje usando la velocidad de su RigidBody, obteniendo los Inputs de los ejes de movimiento
         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
 
-        //    }
-        //    else
-        //    {
-        //        theRB.velocity = Vector2.zero;
-        //    }
-
-
-        //    myAnim.SetFloat("moveX", theRB.velocity.x);
-        //    myAnim.SetFloat("moveY", theRB.velocity.y);
-
-        //    if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
-        //    {
-        //        if (canMove)
-        //        {
-        //            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
-        //            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
-        //        }
-        //    }
-
-        //    transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
+        //ANIMACIONES
+        anim.SetFloat("moveX", theRB.velocity.x);
+        anim.SetFloat("moveY", theRB.velocity.y);
     }
 
-    public void SetBounds(Vector3 botLeft, Vector3 topRight)
-    {
-    //    bottomLeftLimit = botLeft + new Vector3(.5f, 1f, 0f);
-    //    topRightLimit = topRight + new Vector3(-.5f, -1f, 0f);
-    }
 }
